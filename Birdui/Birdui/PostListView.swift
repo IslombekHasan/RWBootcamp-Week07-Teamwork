43d7a55d9931585c40aa9da31bc0836d7ec41b24
Janin Culhaoglu <janin.culhaoglu@gmail.com>
2020-07-12 12:46:02 +0200

    Added the header view of postList and state with boolean for modal sheet

--- Birdui/Birdui/PostListView.swift
+++ Birdui/Birdui/PostListView.swift
@@ -9,10 +9,56 @@
 import SwiftUI
 
 struct PostListView: View {
+    @State var modalIsPresented = false
+
+    struct HeaderTitleTextStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .foregroundColor(Color.black)
+                .font(.system(size: 17))
+        }
+    }
+
+    struct ButtonTextStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .font(.system(size: 15))
+        }
+    }
+
     var body: some View {
-        // TODO: This should look exactly like the Birdie table view,
-        // but with only one button.
-        Text("Layout header, new-post button, List of posts")
+        NavigationView {
+        VStack(alignment: .leading) {
+            let leftImageSize = 50
+            HStack {
+                Image("mascot_swift-badge")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                Spacer()
+                Text("Home")
+                    .modifier(HeaderTitleTextStyle())
+                    .padding(.leading, -CGFloat(leftImageSize))
+                Spacer()
+            }
+            HStack {
+                Button(action: { self.modalIsPresented = true }) {
+                    Text("Create New post")
+                        .modifier(ButtonTextStyle())
+                }
+            }
+            List {
+                Text("Layout header, new-post button, List of posts")
+            }
+            .navigationBarTitle("")
+            .navigationBarHidden(true)
+        }
+        .padding(.top, 16)
+        .padding(.leading, 24)
+        .padding(.trailing, 24)
+    }
+        .sheet(isPresented: $modalIsPresented) {
+            // Display New Post View
+        }
     }
 }
 
